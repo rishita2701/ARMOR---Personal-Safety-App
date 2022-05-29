e86ca95f441a41ddd17af1edff4875a9919f891e
Rishita Mukherjee <rishitam2701@gmail.com>
2022-05-29 15:02:47 +0530

    adding files

---
+++ app/src/main/java/com/personalsafety/armor/FirstAidTip.java
@@ -0,0 +1,16 @@
+package com.personalsafety.armor;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class FirstAidTip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_aid_tip);
+    }
+
+
+}
